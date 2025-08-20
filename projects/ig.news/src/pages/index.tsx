8f3d8f2ca1546d76b209408ba7964204ff35454a
Marco Aurélio <sgtmarcoaurelio@icloud.com>
2025-08-19 23:43:10 -0300

    feat: setup css ig.news

--- projects/ig.news/src/pages/index.tsx
+++ projects/ig.news/src/pages/index.tsx
@@ -1,9 +1,14 @@
-import styles from '../styles/home.module.scss'
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      Ig.<span>news</span>
-    </h1>
-  )
+    <>
+      <Head>
+        <title>Início | Ig.news</title>
+      </Head>
+      <h1>
+        Ig.<span>news</span>
+      </h1>
+    </>
+  );
 }
