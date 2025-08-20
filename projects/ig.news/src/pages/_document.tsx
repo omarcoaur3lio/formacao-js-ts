ebcdbb7b46f7a9c304882593448c5638ad91e415
Marco Aurélio <sgtmarcoaurelio@icloud.com>
2025-08-20 00:43:46 -0300

    feat: add header component in ig.news project

--- projects/ig.news/src/pages/_document.tsx
+++ projects/ig.news/src/pages/_document.tsx
@@ -1,19 +1,22 @@
-import Document, {Html, Head, Main, NextScript} from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
-    return(
+    return (
       <Html>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
-        <title>Ig.news</title>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
       </Html>
-    )
+    );
   }
 }
